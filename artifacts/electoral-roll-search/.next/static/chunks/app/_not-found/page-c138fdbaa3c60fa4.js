2cf9459cb8af4c96791ad2a59d2a0043ca0e13eb
Vijaysinhh <vijaysinhjadhav23@gmail.com>
2026-09-03 19:01:56 +0530

    next.js cleaning

--- artifacts/electoral-roll-search/.next/static/chunks/app/_not-found/page-c138fdbaa3c60fa4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[13,492],{967:()=>{}},_=>{_.O(0,[41,621,358],()=>_(_.s=967)),_N_E=_.O()}]);
--- artifacts/electoral-roll-search/.next/static/chunks/app/not-found-7cff02fae60446ea.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[345],{9335:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4697,23))}},e=>{e.O(0,[697,41,621,358],()=>e(e.s=9335)),_N_E=e.O()}]);
